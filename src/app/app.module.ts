36cfeca8e7c32e82449e62bec33d85ffd06477e7
Axel Alvarez <axjavier4alvarez44@gmail.com>
2024-09-07 15:32:44 -0600

    agregando graficas

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { EjemploComponent } from './components/ejemplo/ejemplo.component';
+import { FormsModule } from '@angular/forms';
+import { ChartsModule } from '@rinminase/ng-charts';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    NavbarComponent,
+    EjemploComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ChartsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
